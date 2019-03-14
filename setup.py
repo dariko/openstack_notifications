cfb843329a9aa9f7a82bc740904308f556670374
dariko <git@dariozanzico.com>
2019-03-14 13:09:51 +0100

    add cli tool

--- setup.py
+++ setup.py
@@ -6,4 +6,8 @@ setup(name='openstack_events',
       version='0.1.0',
       packages=['openstack_events'],
       install_requires=['kombu==4.4.0'],
+      entry_points={
+        'console_scripts': [
+            'openstack_events_monitor = openstack_events.tool:openstack_events_monitor',
+        ]},
       )
